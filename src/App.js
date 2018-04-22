65f325e8da9d5fd4cb08a495e30548be6135df92
shaisachs <ssachs@ngpvan.com>
2018-04-22 14:39:50 -0400

    Make the site a little prettier, cover corner case where today is a holiday
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import rrule from 'rrule';
 import lodash from 'lodash';
 
+function getDateDesc(date) {
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  var year = date.getFullYear();
+  var month = months[date.getMonth()];
+  var dayOfWeek = weekdays[date.getDay()];
+  var dayOfMonth = date.getDate();
+
+  return dayOfWeek + ', ' + month + ' ' + dayOfMonth + ', ' + year;
+}
 
 class App extends Component {
   constructor () {
@@ -12,9 +23,9 @@ class App extends Component {
 
   componentDidMount() {
     var that = this;
-    fetch('https://shaisachs.github.io/ecodays/api.json').
-      then(response => response.json()).
-      then(holidaysData => that.setState({ holidays: holidaysData.holidays }) );
+    fetch('https://shaisachs.github.io/ecodays/api.json')
+      .then(response => response.json())
+      .then(holidaysData => that.setState({ holidays: holidaysData.holidays }) );
   }
 
   renderHolidays() {
@@ -22,9 +33,8 @@ class App extends Component {
       return (<p>Error while rendering days</p>);
     }
 
-    var rrulestr = rrule.rrulestr;
-
     var today = new Date();
+    today.setDate(today.getDate() - 1);
     var inOneYear = new Date();
     inOneYear.setFullYear(inOneYear.getFullYear() + 1);
 
@@ -44,26 +54,36 @@ class App extends Component {
     var sortedOccurrences = lodash.sortBy(compactOccurrences,
       o => o.nextOccurrence.toISOString());
 
-    var answer = lodash.map(sortedOccurrences, function(o) {
-      var y = o.nextOccurrence.getFullYear();
-      var m = o.nextOccurrence.getMonth() + 1;
-      var d = o.nextOccurrence.getDate();
-
+    var answer = lodash.map(sortedOccurrences, function(o, ind) {
       var url = o.holiday.url;
       var name = url ?
         (<a href={url}>{o.holiday.name}</a>) :
         o.holiday.name;
+      var dateDesc = getDateDesc(o.nextOccurrence);
 
-      return (<p>{y}/{m}/{d}: {name}</p>);
+      return (
+        <div class="holiday" key="{ind}">
+          <div class="holiday-name">{name}</div>
+          <div class="holiday-date">{dateDesc}</div>
+        </div>);
     });
 
     return answer;
   }
 
   render() {
-    return this.state.holidays.length ?
-      this.renderHolidays() :
-      (<p>Loading...</p>);
+    if (this.state.holidays.length) {
+      var holidays = this.renderHolidays();
+      return (
+        <div class="app">
+          <h1>Upcoming Environmental Holidays</h1>
+          <div class="holidays">{holidays}</div>
+        </div>
+      );
+    }
+    else {
+      return (<p>Loading...</p>);      
+    }
   }
 }
 
